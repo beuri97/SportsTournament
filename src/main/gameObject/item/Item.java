0304551a381318baaf453a94daccd827a4cce173
BrilliantGuruK <joshua.1.9.kim@gmail.com>
2023-04-20 21:00:04 +1200

    editted doc

--- src/main/gameObject/item/Item.java
+++ src/main/gameObject/item/Item.java
@@ -1,48 +1,70 @@
 package main.gameObject.item;
 
 import main.gameObject.Product;
-
+/**
+ * Item which will be used while playing game
+ *
+ * @author J Kim
+ */
 public class Item implements Product{
 	private String name;
 	private float buyPrice;
 	private int incAmount;
 	private String incStat;
 	
-	
-	public Item(String name, float buy_price, int inc_amount, String inc_stat) {
+	/**
+	 * 
+	 * @param name 
+	 * @param buyPrice - price to buy
+	 * @param incAmount - the amount increasing specific stat by item
+	 * @param incStat - the stat which will be increased by item
+	 */
+	public Item(String name, float buyPrice, int incAmount, String incStat) {
 		this.name = name;
-		this.buyPrice = buy_price;
-		this.incAmount = inc_amount;
-		this.incStat = inc_stat;
+		this.buyPrice = buyPrice;
+		this.incAmount = incAmount;
+		this.incStat = incStat;
 		
 	}
 	
-	
+	/**
+	 * @return the name of item
+	 */
 	public String getName() {
 		
 		return name;
 	}
-	
+	/**
+	 * @return the price of item to buy
+	 */
 	public float getPrice() {
 	
 		return buyPrice;
 	}
-	
+	/**
+	 * @return the price of item to sell
+	 */
 	public float getSellPrice() {
 		float i = getPrice() * SELL_PRICE_PENALTY;		
 		return i;
 	}
-	
-	public int getInc_Amount() {
+	/**
+	 * @return the amount increasing specific stat by item
+	 */
+	public int getIncAmount() {
 		
 		return incAmount;
 	}
-	public String getInc_Stat() {
+	/**
+	 * @return the stat which will be increased by item
+	 */
+	public String getIncStat() {
 		
 		return incStat;
 	}
-	
-	
+	/**
+	 * @return description of item
+	 */
 	public String toString() {
 		
 		return String.format("item: %s%nEffect: %s +%d%nprice: %.2f%n%n", 
---
+++ src/test/MarketTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MarketTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
