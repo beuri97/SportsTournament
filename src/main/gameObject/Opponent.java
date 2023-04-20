d612f9e25321c508f87cc38b8e7b4c88a2e4493e
beuri97 <beuri97@gmail.com>
2023-04-21 03:13:46 +1200

    add Match and Oppoenet Class

---
+++ src/main/gameObject/Opponent.java
@@ -0,0 +1,14 @@
+package main.gameObject;
+
+/**
+ * Class for create opponent Team(NPC)
+ * @author H Yang
+ */
+public class Opponent extends Team{
+
+    public Opponent() {
+        
+    }
+
+
+}
