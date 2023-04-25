cd88e822ca9dc1f0a50988a607e601ec01980da2
beuri97 <beuri97@gmail.com>
2023-04-25 18:48:30 +1200

    updated/modified

--- src/main/gamesystem/Exception/IllegalInputException.java
+++ src/main/gamesystem/Exception/IllegalInputException.java
@@ -2,7 +2,12 @@ package main.gamesystem.Exception;
 
 public class IllegalInputException extends IllegalStateException{
 
-    public IllegalInputException() {}
+    public IllegalInputException() {
+        super("""
+                ERROR: Invalid Input!
+                Please do it again.
+                """);
+    }
     public IllegalInputException(String message) {
 
         super("ERROR: Invalid Input!\n" + message + "\nPlease do it again.\n");
