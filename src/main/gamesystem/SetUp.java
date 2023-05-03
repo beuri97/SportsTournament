376b15d27c6f54c54a370ee6ae8b012f7273e79f
beuri97 <beuri97@gmail.com>
2023-05-03 16:52:14 +1200

    updated: move some source lines from GameEnvironment to SetUp

--- src/main/gamesystem/SetUp.java
+++ src/main/gamesystem/SetUp.java
@@ -1,7 +1,11 @@
 package main.gamesystem;
 
 import main.gameObject.Product;
+import main.gameObject.Team;
+import main.gameObject.athletes.Athlete;
+import main.gamesystem.Exception.EmptySlotException;
 import main.gamesystem.Exception.IllegalInputException;
+import main.gamesystem.Exception.NoSpaceException;
 
 public class SetUp {
 
@@ -10,6 +14,39 @@ public class SetUp {
         if(!input.matches(REGEX)) {
             throw new IllegalInputException(message);
         }
+    }
+
+    public void tradingManager(Team team, Market market, String type, Product[] stock, int col) {
+
+
+
+        switch(type) {
+            case "buy":
+                Product[] properties = (stock instanceof Athlete[]) ? team.getRoster() : team.getInventory();
+                if(stock[col] == null) throw new EmptySlotException();
+                if(team.isFull(properties)) throw new NoSpaceException();
+                team.setMoney(- stock[col].getPrice());
+                Product product = market.purchase(stock, col);
+                if (product instanceof Athlete) {
+                    team.recruitAthletes(product);
+                } else {
+                    team.addItem(product);
+                }
+                break;
+
+            case "sell":
+                if(stock[col] == null) throw new EmptySlotException();
+                Product sale = stock[col];
+                team.setMoney(sale.getPrice());
+                if(stock[col] instanceof Athlete) team.leaveAthletes(col);
+                else team.removeItem(col);
+                break;
+        }
+    }
+
+    public void createOpponent(Team[] opponent) {
+
+
     }
 
     public boolean isFull(Product[] products) {
